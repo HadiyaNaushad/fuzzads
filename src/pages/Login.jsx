7744a2a5dcd1318cdef519b936f1f8b319a61f30
HadiyaNaushad <hadiyanaushad8412@gmail.com>
2026-02-24 16:14:43 +0530

    Updated API URL

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,7 +13,7 @@ const handleLogin = async (e) => {
 e.preventDefault();
 
 try {  
-  const res = await axios.post("http://localhost:5000/api/auth/login", {  
+  const res = await axios.post("https://fuzzads-vq44.onrender.com/api/auth/login", {  
     email: email.toLowerCase().trim(),  
     password: password.trim(),  
   });  
