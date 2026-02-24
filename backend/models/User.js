36345379469c934c3abc6863829d5cd8c7d6ee57
Hadiya Naushad <hadiyanaushad8412@gmail.com>
2026-02-24 11:49:40 +0530

    Refactor User model to include required fields
    Uncommented and updated userSchema definition with required field.

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,19 +1,3 @@
-// import mongoose from "mongoose";
-
-// const userSchema = new mongoose.Schema({
-//   name: String,
-//   email: String,
-//   password: String,
-//   image: {
-//     type: String,
-//     default: "https://via.placeholder.com/120"
-//   },
-//   totalSpend: { type: Number, default: 0 },
-//   totalOrders: { type: Number, default: 0 },
-//   totalBalance: { type: Number, default: 0 }
-// });
-
-// export default mongoose.model("User", userSchema);
 
 
 
